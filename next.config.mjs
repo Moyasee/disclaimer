d08e59237cc80dfb964b9232c23bd203436fc3be
zxcsix-zxc <terranoiz@icloud.com>
2025-04-21 21:54:13 +0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/disclaimer',
+    basePath: '',
     images: {
         unoptimized: true,
     },
